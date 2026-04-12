5d4f3146d968cd8aefe52eb0f76ead4ae719083d
samtimeinc <samtime.worldwide@gmail.com>
2026-04-11 19:27:37 -0700

    saving before lightning destroys everything

--- app/(post-login)/settings/page.tsx
+++ app/(post-login)/settings/page.tsx
@@ -6,13 +6,34 @@ import { useSelector } from 'react-redux'
 import { RootState } from '@/lib/redux/store'
 
 const SettingsPage = () => {
-    const user = useSelector((state: RootState) => state.auth.user);
-    const isAuthLoading = useSelector((state: RootState) => state.auth.loading);
-    
+  const user = useSelector((state: RootState) => state.auth.user);
+  const tier = useSelector((state: RootState) => state.subscription.tier);
+  const isAuthLoading = useSelector((state: RootState) => state.auth.loading);
+        
   return (
     <div className={styles["container"]}>
         <div className={styles["row"]}>
+          <div className={styles["settings__title"]}>Settings</div>
 
+          <div className={styles["settings__content"]}>
+            <div>
+              <div className={styles["settings__subtitle"]}>Subscription Plan</div>
+              <div 
+                className={`
+                  ${styles["settings__text"]} 
+                  ${styles["display__tier"]}
+                `}
+              >
+                {tier}
+              </div>
+            </div>
+          </div>
+
+          <div className={styles["settings__content"]}>
+            <div className={styles["settings__subtitle"]}>Email</div>
+            <div className={styles["settings__text"]}>{user?.email}</div>
+          </div>
+          
         </div>
     </div>
   )
